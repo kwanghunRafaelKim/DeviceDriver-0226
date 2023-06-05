e091013acc7c8a919be3f1ef8b75c6bc70e32049
kwanghun <lighthun@gmail.com>
2023-06-05 14:07:18 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DeviceDriver Test
+
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "../Project_DeviceDriver/DeviceDriver.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
