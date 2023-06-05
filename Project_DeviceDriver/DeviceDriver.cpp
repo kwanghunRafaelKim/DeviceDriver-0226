a6517622fda6452dd15801d5b2a0b45614f84bcd
kwanghun <lighthun@gmail.com>
2023-06-05 16:05:26 +0900

    Second Commit

--- Project_DeviceDriver/DeviceDriver.cpp
+++ Project_DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,23 @@
 #include "DeviceDriver.h"
 
+#include <exception>
+#include <windows.h>
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int FirstReadval = (int)(m_hardware->read(address));
+    Sleep(200); //200ms
+    for(int trycnt = 2; trycnt <= 5; trycnt++)
+    {
+        int curReadval = (int)(m_hardware->read(address));
+        Sleep(200); //200ms
+        if (FirstReadval != curReadval)
+            throw std::exception("Exception!!");
+    }
+    return FirstReadval;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,7 +3,36 @@
 
 #include "../Project_DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+using namespace std;
+using namespace testing;
+class MockFlashMemoryDevice:public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), ());
+	MOCK_METHOD(void, write, (long address, unsigned char data), ());
+};
+
+TEST(TestCaseName, TestName_Read5) {
+	MockFlashMemoryDevice FlashMemoryDevice{};
+
+	EXPECT_CALL(FlashMemoryDevice, read(0x40000000)).
+	Times(5);
+	DeviceDriver driver(&FlashMemoryDevice);
+	driver.read(0x40000000);
+}
+
+TEST(TestCaseName, TestName_Exception) {
+	MockFlashMemoryDevice FlashMemoryDevice{};
+
+
+	EXPECT_CALL(FlashMemoryDevice, read(0x40000000))
+		.WillOnce(Return(5))
+		.WillOnce(Return(5))
+		.WillOnce(Return(5))
+		.WillOnce(Return(5))
+		.WillOnce(Return(6));
+
+	DeviceDriver driver(&FlashMemoryDevice);
+	EXPECT_THROW(driver.read(0x40000000), std::exception);
 }
